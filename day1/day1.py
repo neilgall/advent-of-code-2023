7028d43e83ce3aa8e8cc1011e4d7bf14e835fb3e
Neil Gall <neil@gall.scot>
2023-12-02 20:10:06 +0000

    Day 1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,22 +1,46 @@
-from typing import Iterable
+from functools import reduce
+from parsy import any_char, digit, peek, string
+from typing import Callable, Iterable
 
 Input = list[int]
 
 
-def parse_line(input: str) -> Input:
+def parse_line_part1(input: str) -> Input:
     return [int(c) for c in input if c.isdigit()]
+
+
+def parse_line_part2(input: str) -> Input:
+    words = [
+        (string(w[:-1]) >> peek(string(w[-1]))).result(v)
+        for w, v in {
+            "one": 1,
+            "two": 2,
+            "three": 3,
+            "four": 4,
+            "five": 5,
+            "six": 6,
+            "seven": 7,
+            "eight": 8,
+            "nine": 9,
+        }.items()
+    ]
+    word_parser = reduce(lambda p1, p2: p1 | p2, words)
+    parser = (digit.map(int) | word_parser | any_char.result(None)).many()
+
+    return [p for p in parser.parse(input) if p]
 
 
 def calibration_value(input: Input) -> int:
     return input[0] * 10 + input[-1]
 
 
-def part1(input: Iterable[Input]) -> int:
-    return sum(calibration_value(parse_line(i)) for i in input)
+def calculate(input: Iterable[Input], parse: Callable[[str], Input]) -> int:
+    return sum(calibration_value(parse(i)) for i in input)
 
 
 if __name__ == "__main__":
     with open("input.txt", "rt") as f:
-        input = f.readlines()
+        input = [l.strip() for l in f.readlines()]
 
-    print(f"Part 1: {part1(input)}")
+    print(f"Part 1: {calculate(input, parse_line_part1)}")
+    print(f"Part 2: {calculate(input, parse_line_part2)}")
--- day1/test_day1.py
+++ day1/test_day1.py
@@ -1,27 +1,61 @@
 import pytest
-from day1 import parse_line, calibration_value, Input, part1
+from day1 import parse_line_part1, parse_line_part2, calibration_value, Input, calculate
 
 
-@pytest.mark.parametrize("input,expect", [
-    ("1abc2", [1,2]),
-    ("pqr3stu8vwx", [3,8]), 
-    ("a1b2c3d4e5f", [1,2,3,4,5]),
-    ("treb7uchet", [7])
-])
-def test_parse_line(input: str, expect: list[int]):
-    assert parse_line(input) == expect
+@pytest.mark.parametrize(
+    "input,expect",
+    [
+        ("1abc2", [1, 2]),
+        ("pqr3stu8vwx", [3, 8]),
+        ("a1b2c3d4e5f", [1, 2, 3, 4, 5]),
+        ("treb7uchet", [7]),
+    ],
+)
+def test_parse_line_part1(input: str, expect: list[int]):
+    assert parse_line_part1(input) == expect
 
 
-@pytest.mark.parametrize("input,expect", [
-    ([1,2], 12),
-    ([3,8], 38), 
-    ([1,2,3,4,5], 15),
-    ([7], 77)
-])
+@pytest.mark.parametrize(
+    "input,expect",
+    [
+        ("two1nine", [2, 1, 9]),
+        ("eightwothree", [8, 2, 3]),
+        ("abcone2threexyz", [1, 2, 3]),
+        ("xtwone3four", [2, 1, 3, 4]),
+        ("4nineeightseven2", [4, 9, 8, 7, 2]),
+        ("zoneight234", [1, 8, 2, 3, 4]),
+        ("7pqrstsixteen", [7, 6]),
+    ],
+)
+def test_parse_line_part2(input: str, expect: list[int]):
+    assert parse_line_part2(input) == expect
+
+
+@pytest.mark.parametrize(
+    "input,expect", [([1, 2], 12), ([3, 8], 38), ([1, 2, 3, 4, 5], 15), ([7], 77)]
+)
 def test_calibration_value(input: Input, expect: int):
     assert calibration_value(input) == expect
 
 
 def test_part1():
-    result = part1(["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"])
+    result = calculate(
+        ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"], parse_line_part1
+    )
     assert result == 142
+
+
+def test_part2():
+    result = calculate(
+        [
+            "two1nine",
+            "eightwothree",
+            "abcone2threexyz",
+            "xtwone3four",
+            "4nineeightseven2",
+            "zoneight234",
+            "7pqrstsixteen",
+        ],
+        parse_line_part2,
+    )
+    assert result == 281
