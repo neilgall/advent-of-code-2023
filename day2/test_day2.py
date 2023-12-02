e8d62bec4116bfb0364588f8672ecbca06e22bb9
Neil Gall <neil@gall.scot>
2023-12-02 21:04:04 +0000

    Extra test cases to check multi-digit integers

--- day2/test_day2.py
+++ day2/test_day2.py
@@ -12,6 +12,22 @@ from .__main__ import *
                 { Colour.RED: 1, Colour.GREEN: 2, Colour.BLUE: 6 },
                 { Colour.GREEN: 2}
             ])
+        ),
+        (
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            Game(id=2, turns=[
+                { Colour.BLUE: 1, Colour.GREEN: 2 },
+                { Colour.BLUE: 4, Colour.GREEN: 3, Colour.RED: 1 },
+                { Colour.BLUE: 1, Colour.GREEN: 1 }
+            ])
+        ),
+        (
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            Game(id=3, turns=[
+                { Colour.GREEN: 8, Colour.BLUE: 6, Colour.RED: 20 },
+                { Colour.GREEN: 13, Colour.BLUE: 5, Colour.RED: 4 },
+                { Colour.GREEN: 5, Colour.RED: 1 }
+            ])
         )
     ],
 )
