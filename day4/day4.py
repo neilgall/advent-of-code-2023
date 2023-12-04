10555cc9f90103dcce524213987c793eb434aefe
Neil Gall <neil@gall.scot>
2023-12-04 11:51:43 +0000

    Day 4 part 2

--- day4/day4.py
+++ day4/day4.py
@@ -17,9 +17,29 @@ class Card:
         card = seq(card_id, integer_list, separator >> integer_list).combine(Card)
         return card.parse(input)
 
+    def count_matching(self):
+        return sum(1 for n in self.mine if n in self.winning)
+
     def point_value(self):
-        matches = sum(1 for n in self.mine if n in self.winning)
+        matches = self.count_matching()
         return 0 if matches == 0 else 2 ** (matches - 1)
+
+
+@dataclass
+class CardSet:
+    cards: list[Card]
+    counts: dict[int, int]
+
+    def __init__(self, cards: list[Card]):
+        self.cards = cards
+        self.counts = { (card.id): 1 for card in cards }
+
+    def total(self) -> int:
+        for card in self.cards:
+            for n in range(0, card.count_matching()):
+                if card.id+1+n in self.counts:
+                    self.counts[card.id+1+n] += self.counts[card.id]
+        return sum(self.counts.values())
 
 
 def part1(input: str) -> int:
@@ -28,4 +48,5 @@ def part1(input: str) -> int:
 
 
 def part2(input: str) -> int:
-    return 0
+    cards = [Card.parse(line) for line in input.strip().split("\n")]
+    return CardSet(cards).total()
--- day4/test_day4.py
+++ day4/test_day4.py
@@ -1,6 +1,14 @@
 import pytest
 from .day4 import *
 
+EXAMPLE = [
+    "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+    "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+    "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+    "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+    "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+    "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+]
 
 @pytest.mark.parametrize(
     "input,expect",
@@ -26,15 +34,7 @@ def test_parse(input: str, expect: Card):
 
 
 @pytest.mark.parametrize(
-    "input,expect",
-    [
-        ("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", 8),
-        ("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19", 2),
-        ("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1", 2),
-        ("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83", 1),
-        ("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36", 0),
-        ("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11", 0),
-    ],
+    "input,expect", zip(EXAMPLE, [8, 2, 2, 1, 0, 0])
 )
 def test_point_value(input, expect):
     card = Card.parse(input)
@@ -42,16 +42,14 @@ def test_point_value(input, expect):
 
 
 def test_part1():
-    assert (
-        part1(
-            """
-Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
-"""
-        )
-        == 13
-    )
+    assert part1("\n".join(EXAMPLE)) == 13
+
+
+def test_card_set():
+    card_set = CardSet([Card.parse(c) for c in EXAMPLE])
+    assert card_set.counts == { 1:1, 2:1, 3:1, 4:1, 5:1, 6:1 }
+
+
+def test_part2():
+    assert part2("\n".join(EXAMPLE)) == 30
+    
