a058a2884cbbbe10bed3609e91fd0878b0095af8
Neil Gall <neil@gall.scot>
2023-12-06 08:39:35 +0000

    Generate test skeletons in new.py

--- new.py
+++ new.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 if __name__ == "__main__":
     today = f"day{datetime.now().day}"
-    dir = Path(today)
+    dir = "test" / Path(today)
 
     dir.mkdir(parents=True)
     (dir / "__init__.py").touch()
@@ -16,19 +16,32 @@ def part1(input: str) -> int:
     return 0
 
 
-def part2(input:str) -> int:
+def part2(input: str) -> int:
     return 0
 """
     )
 
     (dir / f"test_{today}.py").write_text(
-        f"""import pytest
+        f'''import pytest
 from .{today} import *
+
+
+EXAMPLE = """
 """
+
+
+def test_part1():
+    assert part1(EXAMPLE) == 0
+
+
+def test_part2():
+    assert part2(EXAMPLE) == 0
+'''
     )
 
     (dir / "__main__.py").write_text(
         f"""from {today} import part1, part2
+
 
 if __name__ == "__main__":
     with open("{today}/input.txt", "rt") as f:
