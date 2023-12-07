63ee0ca4ee60ed45b7895312e9d39c32614dcbc6
Neil Gall <neil@gall.scot>
2023-12-07 19:49:56 +0000

    Day 7 part 2 - tests pass, wrong answer

--- day7/day7.py
+++ day7/day7.py
@@ -1,12 +1,24 @@
 from collections import Counter
 from dataclasses import dataclass
 from enum import Enum
-from functools import total_ordering
-from parsy import char_from, decimal_digit, seq, whitespace
+from functools import partial, total_ordering
+from parsy import decimal_digit, seq, string_from, whitespace
 
 
 Card = str
-CARDS = "23456789TJQKA"
+
+
+@dataclass
+class Config:
+    ranks: list[Card]
+    jokers: bool
+
+    def rank(self, card: Card):
+        return self.ranks.index(card)
+
+
+PART1_CONFIG = Config(list("23456789TJQKA"), False)
+PART2_CONFIG = Config(list("J23456789TQKA"), True)
 
 
 class Type(Enum):
@@ -25,54 +37,88 @@ class Type(Enum):
 @total_ordering
 @dataclass
 class Hand:
+    config: Config
     cards: list[Card]
     bid: int
 
-    def __post__init(self):
-        assert all(c in CARDS for c in self.cards)
-
+    def __post_init__(self):
+        assert len(self.cards) == 5
+        assert all(c in self.config.ranks for c in self.cards)
 
     @property
     def type(self) -> Type:
-        counts: list[int] = Counter(self.cards).values()
+        counter = Counter(self.cards)
+        if self.config.jokers:
+            jokers = counter["J"]
+            del counter["J"]
+        else:
+            jokers = 0
+        counts = counter.values()
+
         if 5 in counts:
             return Type.FIVE_OF_A_KIND
+
         elif 4 in counts:
-            return Type.FOUR_OF_A_KIND
+            match jokers:
+                case 1: return Type.FIVE_OF_A_KIND
+                case _: return Type.FOUR_OF_A_KIND
+
         elif 3 in counts:
-            return Type.FULL_HOUSE if 2 in counts else Type.THREE_OF_A_KIND
-        elif sum(1 for c in counts if c == 2) == 2:
-            return Type.TWO_PAIR
+            match jokers:
+                case 2: return Type.FIVE_OF_A_KIND
+                case 1: return Type.FOUR_OF_A_KIND
+                case _: return Type.FULL_HOUSE if 2 in counts else Type.THREE_OF_A_KIND
+
+        elif sum(1 for n in counts if n == 2) == 2:
+            match jokers:
+                case 1: return Type.FULL_HOUSE
+                case _: return Type.TWO_PAIR
+
         elif 2 in counts:
-            return Type.ONE_PAIR
+            match jokers:
+                case 3: return Type.FIVE_OF_A_KIND
+                case 2: return Type.FOUR_OF_A_KIND
+                case 1: return Type.THREE_OF_A_KIND
+                case _: return Type.ONE_PAIR
         else:
-            return Type.HIGH_CARD
+            match jokers:
+                case 4: return Type.FIVE_OF_A_KIND
+                case 3: return Type.FOUR_OF_A_KIND
+                case 2: return Type.THREE_OF_A_KIND
+                case 1: return Type.ONE_PAIR
+                case _: return Type.HIGH_CARD
         
+
     def __lt__(self, other: "Hand") -> int:
         d = self.type.value - other.type.value
         if d < 0: return True
         if d > 0: return False
+
         for c1, c2 in zip(self.cards, other.cards):
-            d = CARDS.index(c1) - CARDS.index(c2)
+            d = self.config.rank(c1) - self.config.rank(c2)
             if d < 0: return True
             if d > 0: return False
         return False
 
 
-def parse_input(input: str) -> list[Hand]:
-    card = char_from(CARDS)
-    cards = card.times(5)
+def parse_input(input: str, config: Config) -> list[Hand]:
+    cards = string_from(*config.ranks).times(5)
     bid = decimal_digit.at_least(1).map(lambda ds: int("".join(ds)))
-    hand = seq(cards << whitespace, bid).combine(Hand)
+    hand = seq(cards << whitespace, bid).combine(lambda h, b: Hand(config, h, b))
     return hand.sep_by(whitespace).parse(input.strip())
 
 
-def part1(input: str) -> int:
-    hands = parse_input(input)
+def rank_and_score(hands: list[Hand]) -> int:
     hands = sorted(hands)
     scores = [hand.bid * (rank+1) for rank, hand in enumerate(hands)]
     return sum(scores)
 
 
+def part1(input: str) -> int:
+    hands = parse_input(input, PART1_CONFIG)
+    return rank_and_score(hands)
+
+
 def part2(input: str) -> int:
-    return 0
+    hands = parse_input(input, PART2_CONFIG)
+    return rank_and_score(hands)
--- day7/test_day7.py
+++ day7/test_day7.py
@@ -12,13 +12,13 @@ QQQJA 483
 
 
 def test_parser():
-    hands = parse_input(EXAMPLE)
+    hands = parse_input(EXAMPLE, PART1_CONFIG)
     assert hands == [
-        Hand(["3", "2", "T", "3", "K"], 765),
-        Hand(["T", "5", "5", "J", "5"], 684),
-        Hand(["K", "K", "6", "7", "7"], 28),
-        Hand(["K", "T", "J", "J", "T"], 220),
-        Hand(["Q", "Q", "Q", "J", "A"], 483)
+        Hand(PART1_CONFIG, ["3", "2", "T", "3", "K"], 765),
+        Hand(PART1_CONFIG, ["T", "5", "5", "J", "5"], 684),
+        Hand(PART1_CONFIG, ["K", "K", "6", "7", "7"], 28),
+        Hand(PART1_CONFIG, ["K", "T", "J", "J", "T"], 220),
+        Hand(PART1_CONFIG, ["Q", "Q", "Q", "J", "A"], 483)
     ]
 
 
@@ -45,7 +45,36 @@ def test_parser():
     ("23456", Type.HIGH_CARD)
 ])
 def test_type(hand: str, hand_type: Type):
-    assert Hand(list(hand), 0).type == hand_type
+    assert Hand(PART1_CONFIG, list(hand), 0).type == hand_type
+
+
+@pytest.mark.parametrize("hand,hand_type", [
+    ("AAAAA", Type.FIVE_OF_A_KIND),
+    ("JAAAA", Type.FIVE_OF_A_KIND),
+    ("8AAAA", Type.FOUR_OF_A_KIND),
+    ("A8AAA", Type.FOUR_OF_A_KIND),
+    ("T55J5", Type.FOUR_OF_A_KIND),
+    ("KTJJT", Type.FOUR_OF_A_KIND),
+    ("QQQJA", Type.FOUR_OF_A_KIND),
+    ("23332", Type.FULL_HOUSE),
+    ("32332", Type.FULL_HOUSE),
+    ("23323", Type.FULL_HOUSE),
+    ("33232", Type.FULL_HOUSE),
+    ("TTT98", Type.THREE_OF_A_KIND),
+    ("TT9T8", Type.THREE_OF_A_KIND),
+    ("T9T8T", Type.THREE_OF_A_KIND),
+    ("98TTT", Type.THREE_OF_A_KIND),
+    ("23432", Type.TWO_PAIR),
+    ("23234", Type.TWO_PAIR),
+    ("43232", Type.TWO_PAIR),
+    ("KK677", Type.TWO_PAIR),
+    ("A23A4", Type.ONE_PAIR),
+    ("23AA4", Type.ONE_PAIR),
+    ("32T3K", Type.ONE_PAIR),
+    ("23456", Type.HIGH_CARD)
+])
+def test_type_with_jokers(hand: str, hand_type: Type):
+    assert Hand(PART2_CONFIG, list(hand), 0).type == hand_type
 
 
 @pytest.mark.parametrize("hand1,hand2", [
@@ -57,12 +86,15 @@ def test_type(hand: str, hand_type: Type):
     ("23456", "A23A4")
 ])
 def test_ordering(hand1: str, hand2: str):
-    assert Hand(list(hand1), 0) < Hand(list(hand2), 0)
-    assert Hand(list(hand2), 0) > Hand(list(hand1), 0)
+    h1 = Hand(PART1_CONFIG, list(hand1), 0)
+    h2 = Hand(PART1_CONFIG, list(hand2), 0)
+    assert h1 < h2
+    assert h2 > h1
+
 
 def test_part1():
     assert part1(EXAMPLE) == 6440
 
 
 def test_part2():
-    assert part2(EXAMPLE) == 0
+    assert part2(EXAMPLE) == 5905
