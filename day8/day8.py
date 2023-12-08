79f3ab0cec2f9e6e9d1239ca81d40a269350fdd5
Neil Gall <neil@gall.scot>
2023-12-08 15:41:18 +0000

    Day 8 part 2

--- day8/day8.py
+++ day8/day8.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
 from enum import Enum
-from parsy import from_enum, letter, seq, string, whitespace
+from functools import reduce
+from parsy import alt, decimal_digit, from_enum, letter, seq, string, whitespace
+from sympy.ntheory.modular import solve_congruence
+import operator
 
 
 NodeId = str
@@ -19,17 +22,16 @@ class Document:
     @classmethod
     def parse(cls, input: str) -> "Document":
         direction = from_enum(Direction)
-        node_id = letter.at_least(1).map("".join)
+        node_id = alt(letter, decimal_digit).at_least(1).map("".join)
         node = seq(
             node_id << whitespace << string("=") << whitespace,
             seq(
                 string("(") >> node_id << string(",") << whitespace,
-                node_id << string(")")
-            ).map(tuple)
+                node_id << string(")"),
+            ).map(tuple),
         )
         document = seq(
-            direction.at_least(1) << whitespace,
-            node.sep_by(whitespace).map(dict)
+            direction.at_least(1) << whitespace, node.sep_by(whitespace).map(dict)
         ).combine(Document)
         return document.parse(input.strip())
 
@@ -40,20 +42,31 @@ class Document:
         dir = 0 if self.direction(pos) == Direction.LEFT else 1
         return self.network[node][dir]
 
+    def start_nodes(self) -> list[NodeId]:
+        return [id for id in self.network.keys() if id.endswith("A")]
 
-def count_steps(doc: Document) -> int:
-    node = "AAA"
-    pos = 0
-    while node != "ZZZ":
-        node = doc.move(node, pos)
-        pos += 1
-    return pos
+    def count_steps(self, start_node: NodeId) -> tuple[int, int]:
+        node = start_node
+        pos = 0
+        zs = 0
+        initial_steps = 0
+        while zs < 2:
+            node = self.move(node, pos)
+            pos += 1
+            if node.endswith("Z"):
+                if zs == 0:
+                    initial_steps = pos
+                zs += 1
+        return initial_steps, pos - initial_steps
 
 
 def part1(input: str) -> int:
     document = Document.parse(input)
-    return count_steps(document)
+    steps, loop = document.count_steps("AAA")
+    return steps
 
 
 def part2(input: str) -> int:
-    return 0
+    document = Document.parse(input)
+    solution = solve_congruence(*(document.count_steps(id) for id in document.start_nodes()))
+    return solution[1]
--- day8/test_day8.py
+++ day8/test_day8.py
@@ -22,31 +22,51 @@ BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)
 """
 
+EXAMPLE3 = """
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+"""
+
 
 def test_parser():
     document = Document.parse(EXAMPLE)
     assert document == Document(
-        directions = [Direction.RIGHT, Direction.LEFT],
-        network = {
+        directions=[Direction.RIGHT, Direction.LEFT],
+        network={
             "AAA": ("BBB", "CCC"),
             "BBB": ("DDD", "EEE"),
             "CCC": ("ZZZ", "GGG"),
             "DDD": ("DDD", "DDD"),
             "EEE": ("EEE", "EEE"),
             "GGG": ("GGG", "GGG"),
-            "ZZZ": ("ZZZ", "ZZZ")
-        }
+            "ZZZ": ("ZZZ", "ZZZ"),
+        },
     )
 
 
+def test_start_nodes():
+    document = Document.parse(EXAMPLE3)
+    assert document.start_nodes() == ["11A", "22A"]
 
-@pytest.mark.parametrize("input,steps", [
-    (EXAMPLE, 2),
-    (EXAMPLE2, 6),
-])
+
+@pytest.mark.parametrize(
+    "input,steps",
+    [
+        (EXAMPLE, 2),
+        (EXAMPLE2, 6),
+    ],
+)
 def test_part1(input, steps):
     assert part1(input) == steps
 
 
 def test_part2():
-    assert part2(EXAMPLE) == 0
+    assert part2(EXAMPLE3) == 6
