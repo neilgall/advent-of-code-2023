7db12d4ba7c7f0704878b43de08fac434a81429c
Neil Gall <neil@gall.scot>
2023-12-11 20:13:48 +0000

    Day 10 part 2 not yet working

--- day10/day10.py
+++ day10/day10.py
@@ -31,6 +31,19 @@ class Pos:
     x: int
     y: int
 
+    def __repr__(self) -> str:
+        return f"({self.x},{self.y})"
+
+    def __mul__(self, n: int) -> "Pos":
+        return Pos(self.x * n, self.y * n)
+
+    def __lt__(self, other: "Pos") -> bool:
+        if self.y < other.y:
+            return True
+        if self.y == other.y and self.x < other.x:
+            return True
+        return False
+
     def go(self, dir: Directions) -> "Pos":
         x = self.x
         y = self.y
@@ -73,6 +86,7 @@ class Tile:
 class Field:
     tiles: dict[Pos, Tile]
     start: Pos
+    extent: Pos
 
     def __post_init__(self):
         if self.start:
@@ -82,15 +96,33 @@ class Field:
     def parse(cls, input: str) -> "Field":
         tiles = {}
         start = None
+        maxx = 0
+        maxy = 0
         for y, line in enumerate(input.strip().split("\n")):
+            maxy = max(y, maxy)
             for x, char in enumerate(line.strip()):
+                maxx = max(x, maxx)
                 tile = Tile.from_symbol(char)
                 if tile:
                     tiles[Pos(x, y)] = tile
                 elif char == "S":
                     start = Pos(x, y)
 
-        return Field(tiles, start or Pos(0, 0))
+        return Field(tiles, start or Pos(0, 0), Pos(maxx, maxy))
+
+    def __str__(self) -> str:
+        out = ""
+        for y in range(0, self.extent.y+1):
+            for x in range(0, self.extent.x+1):
+                d = self.connections(Pos(x, y))
+                out += "X" if d else "."
+            out += "\n"
+        return out
+
+    def positions(self) -> Generator[Pos, None, None]:
+        for y in range(1, self.extent.y):
+            for x in range(1, self.extent.x):
+                yield Pos(x, y)
 
     def connected(self, pos: Pos, direction: Directions) -> bool:
         tile = self.tiles.get(pos)
@@ -107,27 +139,125 @@ class Field:
                 directions |= dir
         return Tile(connected=directions)
 
-    def find_furthest_tile_from_start(self) -> tuple[Pos, int]:
-        def connected_neighbours(pos: Pos) -> Generator[Pos, None, None]:
-            connections = self.connections(pos)
-            for dir in ALL_DIRECTIONS:
-                if dir in connections:
-                    yield pos.go(dir)
+    def connected_neighbours(self, pos: Pos) -> Generator[Pos, None, None]:
+        connections = self.connections(pos)
+        for dir in ALL_DIRECTIONS:
+            if dir in connections:
+                yield pos.go(dir)
 
+    def open_neighbours(self, pos: Pos) -> Generator[Pos, None, None]:
+        for dir in ALL_DIRECTIONS:
+            n = pos.go(dir)
+            if n not in self.tiles:
+                yield n
+
+    def find_distances_from_start(self) -> dict[Pos, int]:
         remaining = [self.start]
         distances = {self.start: 0}
         while remaining:
             pos = remaining.pop(0)
             distance = distances[pos]
-            for n in connected_neighbours(pos):
+            for n in self.connected_neighbours(pos):
                 if n in distances:
                     distances[n] = min(distance + 1, distances[n])
                 else:
                     distances[n] = distance + 1
                     remaining.append(n)
+        return distances
 
+    def find_furthest_tile_from_start(self) -> tuple[Pos, int]:
+        distances = self.find_distances_from_start()
         return max(distances.items(), key=lambda i: i[1])
 
+    def zoom(self) -> "Field":
+        def tiles() -> Generator[tuple[Pos, Tile], None, None]:
+            for pos, tile in self.tiles.items():
+                new_pos = pos * 2
+                yield new_pos, tile
+                if Directions.EAST in tile.connected:
+                    yield new_pos.go(Directions.EAST), Tile(connected=Directions.EAST | Directions.WEST)
+                if Directions.SOUTH in tile.connected:
+                    yield new_pos.go(Directions.SOUTH), Tile(connected=Directions.NORTH | Directions.SOUTH)
+
+        return Field(
+            tiles = dict(tiles()),
+            start = self.start * 2,
+            extent = Pos(self.extent.x * 2 + 1, self.extent.y * 2 + 1)
+        )
+
+    def find_loop(self) -> list[Pos]:
+        distances = self.find_distances_from_start()
+        end, _ = max(distances.items(), key=lambda i: i[1])
+
+        def find_paths_from(
+            pos: Pos, path_to_pos: []
+        ) -> Generator[list[Pos], None, None]:
+            if pos == end:
+                yield [*path_to_pos, pos]
+            else:
+                for n in self.connected_neighbours(pos):
+                    if distances[n] == distances[pos] + 1:
+                        yield from find_paths_from(n, [*path_to_pos, pos])
+
+        paths = find_paths_from(self.start, [])
+        return sorted(set(pos for path in paths for pos in path))
+
+    def is_inside_loop(self, pos: Pos) -> tuple[bool, set[Pos]]:
+        def debug(pos: Pos, checked: set[Pos], unchecked: set[Pos]):
+            out = ""
+            for y in range(0, self.extent.y+1):
+                for x in range(0, self.extent.x+1):
+                    p = Pos(x, y)
+                    if p == pos:
+                        out += "X";
+                    elif p in checked:
+                        out += "@"
+                    elif p in unchecked:
+                        out += "?"
+                    else:
+                        d = self.connections(p)
+                        out += "*" if d else "."
+                out += "\n"
+            return out
+
+        if pos in self.tiles:
+            return False, set()
+
+        unchecked = set([pos])
+        checked = set()
+        while len(unchecked) > 0:
+            next = unchecked.pop()
+            assert next not in checked
+            assert next not in self.tiles
+            if next.x == 0 or next.y == 0 or next.x == self.extent.x or next.y == self.extent.y:
+                # print(f"outside {len(checked)}\n{debug(next, checked, unchecked)}")
+                return False, checked | unchecked
+            else:        
+                checked.add(next)
+                neighbours = set(n for n in self.open_neighbours(next) if n not in checked and not n in self.tiles)
+                unchecked |= neighbours
+        # print(f"inside {len(checked)}\n{debug(next, checked, unchecked)}")
+        return True, checked
+        
+    def positions_enclosed_by_loop(self) -> Generator[Pos, None, None]:
+        determined = set()
+        for pos in self.positions():
+            if pos not in determined:
+                inside, checked = self.is_inside_loop(pos)
+                determined |= checked
+                if inside:
+                    yield from checked
+
+
+    def positions_enclosed_by_loop_with_squeezing(self) -> Generator[Pos, None, None]:
+        field = self.zoom()
+        determined = set()
+        for pos in field.positions():
+            if pos not in determined:
+                inside, checked = field.is_inside_loop(pos)
+                determined |= checked
+                if inside:
+                    yield from (c for c in checked if c.x % 2 == 0 and c.y % 2 == 0)
 
 def part1(input: str) -> int:
     field = Field.parse(input)
@@ -136,4 +266,8 @@ def part1(input: str) -> int:
 
 
 def part2(input: str) -> int:
-    return 0
+    print("part2", input)
+    field = Field.parse(input)
+    inside = list(field.positions_enclosed_by_loop_with_squeezing())
+    print("result ", len(inside))
+    return len(inside)
--- day10/test_day10.py
+++ day10/test_day10.py
@@ -26,6 +26,56 @@ SJLL7
 LJ.LJ
 """
 
+PART2_EXAMPLE = """
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+"""
+
+PART2_EXAMPLE_WITH_SQUEEZING = """
+..........
+.S------7.
+.|F----7|.
+.||OOOO||.
+.||OOOO||.
+.|L-7F-J|.
+.|II||II|.
+.L--JL--J.
+..........
+"""
+
+PART2_LARGE_EXAMPLE = """
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+"""
+
+PART2_LARGE_EXAMPLE2 = """
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+"""
+
 
 def test_parser():
     field = Field.parse(EXAMPLE)
@@ -41,6 +91,7 @@ def test_parser():
             Pos(3, 3): Tile(connected=Directions.NORTH | Directions.WEST),
         },
         start=Pos(0, 0),
+        extent=Pos(4, 4)
     )
 
 
@@ -58,6 +109,7 @@ def test_infer_connections():
             Pos(3, 3): Tile(connected=Directions.NORTH | Directions.WEST),
         },
         start=Pos(1, 1),
+        extent=Pos(4, 4)
     )
 
 
@@ -70,9 +122,86 @@ def test_find_furthest_tile_from_start(input, pos, distance):
     assert field.find_furthest_tile_from_start() == (pos, distance)
 
 
+@pytest.mark.parametrize(
+    "input,positions",
+    [
+        (
+            EXAMPLE_WITH_START,
+            [
+                Pos(1, 1),
+                Pos(2, 1),
+                Pos(3, 1),
+                Pos(1, 2),
+                Pos(3, 2),
+                Pos(1, 3),
+                Pos(2, 3),
+                Pos(3, 3),
+            ],
+        ),
+        (
+            COMPLEX_EXAMPLE,
+            [
+                Pos(2, 0),
+                Pos(3, 0),
+                Pos(1, 1),
+                Pos(2, 1),
+                Pos(3, 1),
+                Pos(0, 2),
+                Pos(1, 2),
+                Pos(3, 2),
+                Pos(4, 2),
+                Pos(0, 3),
+                Pos(1, 3),
+                Pos(2, 3),
+                Pos(3, 3),
+                Pos(4, 3),
+                Pos(0, 4),
+                Pos(1, 4),
+            ],
+        ),
+    ],
+)
+def test_find_loop(input, positions):
+    field = Field.parse(input)
+    assert field.find_loop() == positions
+
+
+def test_is_inside_loop():
+    field = Field.parse(PART2_EXAMPLE)
+    assert field.is_inside_loop(Pos(0, 0)) == (False, set())
+    assert field.is_inside_loop(Pos(2, 6)) == (True, set([Pos(2, 6), Pos(3, 6)]))
+    assert field.is_inside_loop(Pos(3, 6)) == (True, set([Pos(2, 6), Pos(3, 6)]))
+    assert field.is_inside_loop(Pos(7, 6)) == (True, set([Pos(7, 6), Pos(8, 6)]))
+    assert field.is_inside_loop(Pos(8, 6)) == (True, set([Pos(7, 6), Pos(8, 6)]))
+
+
+@pytest.mark.parametrize("input,count", [
+    (PART2_EXAMPLE, 4),
+    (PART2_EXAMPLE_WITH_SQUEEZING, 12),
+])
+def test_positions_enclosed_by_loop(input, count):
+    field = Field.parse(input)
+    positions = list(field.positions_enclosed_by_loop())
+    assert len(positions) == count
+
+
+@pytest.mark.parametrize("input,count", [
+    (PART2_EXAMPLE, 4),
+    (PART2_EXAMPLE_WITH_SQUEEZING, 4),
+])
+def test_positions_enclosed_by_loop_with_squeezing(input, count):
+    field = Field.parse(input)
+    positions = list(field.positions_enclosed_by_loop_with_squeezing())
+    assert len(positions) == count
+
+
 def test_part1():
     assert part1(COMPLEX_EXAMPLE) == 8
 
 
-def test_part2():
-    assert part2(EXAMPLE) == 0
+@pytest.mark.parametrize("input,count", [
+    (PART2_LARGE_EXAMPLE, 8),
+    (PART2_LARGE_EXAMPLE2, 10),
+])
+def test_part2(input, count):
+    assert part2(input) == count
